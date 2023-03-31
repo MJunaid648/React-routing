1ad6ce70f41d35a7550bf2a892af379b697de4d0
Muhammad Junaid <648mjunaid@gmail.com>
2023-03-31 15:58:31 +0500

    all setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+import HomePage from "./HomePage";
+import ProductsPage from "./ProductsPage";
+import AboutPage from "./AboutPage";
+import { Route, Routes, NavLink } from "react-router-dom";
 
-function App() {
+const NavBar = styled.div`
+  border: 2px dotted rgba(45, 94, 13, 1);
+  background-color: aliceblue;
+  margin: 5px;
+  margin-left: auto;
+  margin-right: auto;
+  ul {
+    display: flex;
+    justify-content: center;
+    list-style-type: none;
+  }
+  li {
+    font-weight: bold;
+    display: inline;
+    margin: 5px 10px;
+  }
+  a {
+    text-decoration: none;
+    color: black;
+  }
+
+  ul li a {
+    color: #000;
+  }
+
+  ul li a:hover {
+    color: #00a8ff;
+  }
+
+  ul li a.active {
+    color: #00a8ff;
+  }
+`;
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar>
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/products">Products</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">About-us</NavLink>
+          </li>
+        </ul>
+      </NavBar>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/products" element={<ProductsPage />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/ProductsPage.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+
+const Title = styled.h1`
+  font-size: 40px;
+  text-align: center;
+  font-weight: bold;
+`;
+const Divi = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 15px;
+  button {
+    border-radius: 15px;
+    font-size: 15px;
+    width: 200px;
+    height: 30px;
+    font-weight: 300;
+    background-color: aliceblue;
+    cursor: pointer;
+  }
+  button:hover {
+    color: #00a8ff;
+    font-size: 18px;
+  }
+`;
+const ProductsPage = () => {
+  const navigate = useNavigate();
+  return (
+    <Divi>
+      <Title>Products</Title>
+      <br />
+      <h1>Programatically Navigation using buttons: </h1>
+      <button onClick={() => navigate("/")}>Home</button>
+      <button onClick={() => navigate("/about")}>About-us</button>
+    </Divi>
+  );
+};
+
+export default ProductsPage;
